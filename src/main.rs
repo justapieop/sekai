86494cc6a069d58da85472b8bfe3f545ee052745
Do Tran Thai An <justapie06@gmail.com>
2026-02-27 21:12:40 +0700

    chore: initialization utils

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+mod config;
+mod jwt_utils;
+mod snowflake;
+mod state;
+
 use std::{error::Error, sync::Arc};
 
 use axum::Router;
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, sync::Mutex};
 use tower::ServiceBuilder;
 use tower_http::{
     compression::CompressionLayer, normalize_path::NormalizePathLayer, trace::TraceLayer,
 };
 use tracing::info;
 
-use crate::{config::Config, state::AppState};
-
-mod config;
-mod state;
+use crate::{config::Config, jwt_utils::JwtUtils, snowflake::SnowflakeGenerator, state::AppState};
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -21,8 +23,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     info!("Starting server");
 
+    info!("Creating snowflake generator");
+    let snowflake: Arc<Mutex<SnowflakeGenerator>> =
+        Arc::new(Mutex::new(SnowflakeGenerator::new(config.machine_id)));
+
+    info!("Verifying JWKS");
+    let jwt_utils: Arc<JwtUtils> =
+        Arc::new(JwtUtils::new(&config.jwks_iss, &config.jwks_url).await);
+
     info!("Creating state");
-    let state: Arc<AppState> = Arc::new(AppState::new());
+    let state: Arc<AppState> = Arc::new(AppState::new(snowflake, jwt_utils));
 
     info!("Initializing axum");
     let router: Router<()> = Router::new()
