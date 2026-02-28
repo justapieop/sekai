9096d924ad440b09d147ceec3f4367bcfded7b7c
Do Tran Thai An <justapie06@gmail.com>
2026-02-28 12:47:23 +0700

    chore: fix some migration data

--- migrations/20260227150451_init.down.sql
+++ migrations/20260227150451_init.down.sql
@@ -1,2 +1,4 @@
+DROP TABLE IF EXISTS post_attachments;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS file_metadata;
--- migrations/20260227150451_init.up.sql
+++ migrations/20260227150451_init.up.sql
@@ -1,17 +1,36 @@
+CREATE TABLE IF NOT EXISTS file_metadata (
+    id NUMERIC(39, 0) PRIMARY KEY,
+    file_name VARCHAR(256) NOT NULL
+);
+
+CREATE UNIQUE INDEX file_metadata_idx ON file_metadata(id);
+
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     bio TEXT NOT NULL DEFAULT '',
     is_admin BOOLEAN NOT NULL DEFAULT false,
-    points INT NOT NULL DEFAULT 0
+    points BIGINT NOT NULL DEFAULT 0
 );
+
+CREATE UNIQUE INDEX user_idx ON users(id);
 
 CREATE TABLE IF NOT EXISTS posts (
     id NUMERIC(39, 0) PRIMARY KEY,
     author UUID NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    content TEXT,
-    likes BIGINT DEFAULT 0
+    content TEXT NOT NULL DEFAULT '',
+    likes BIGINT NOT NULL DEFAULT 0
 );
+
+CREATE UNIQUE INDEX post_idx ON posts(id);
+
+CREATE TABLE post_attachments (
+    post_id NUMERIC(39, 0) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    attachment_id NUMERIC(39, 0) REFERENCES file_metadata(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (post_id, attachment_id)
+);
+
+CREATE UNIQUE INDEX post_attachment_idx ON post_attachments(post_id, attachment_id);
